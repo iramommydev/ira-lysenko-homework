63d00fed6173198b7e8027128889dff0936e5006
ira <irysjals0508@gmail.com>
2024-03-25 22:14:03 +0200

    DOM hw

--- JavaScript/DOM/index.js
+++ JavaScript/DOM/index.js
@@ -7,42 +7,42 @@ if (DOMList) {
     span.innerText = text;
     li.appendChild(span);
     DOMList.appendChild(li);
-  }
- 
-const playList = [
-  {
-    author: 'LED ZEPPELIN',
-    song: 'STAIRWAY TO HEAVEN',
-  },
-  {
-    author: 'QUEEN',
-    song: 'BOHEMIAN RHAPSODY',
-  },
-  {
-    author: 'LYNYRD SKYNYRD',
-    song: 'FREE BIRD',
-  },
-  {
-    author: 'DEEP PURPLE',
-    song: 'SMOKE ON THE WATER',
-  },
-  {
-    author: 'JIMI HENDRIX',
-    song: 'ALL ALONG THE WATCHTOWER',
-  },
-  {
-    author: 'AC/DC',
-    song: 'BACK IN BLACK',
-  },
-  {
-    author: 'QUEEN',
-    song: 'WE WILL ROCK YOU',
-  },
-  {
-    author: 'METALLICA',
-    song: 'ENTER SANDMAN',
-  },
-].forEach((item) => {
-    addItemToList(item.author:);
-  });  
+  };
+
+  const playList = [
+    {
+      author: 'LED ZEPPELIN',
+      song: 'STAIRWAY TO HEAVEN',
+    },
+    {
+      author: 'QUEEN',
+      song: 'BOHEMIAN RHAPSODY',
+    },
+    {
+      author: 'LYNYRD SKYNYRD',
+      song: 'FREE BIRD',
+    },
+    {
+      author: 'DEEP PURPLE',
+      song: 'SMOKE ON THE WATER',
+    },
+    {
+      author: 'JIMI HENDRIX',
+      song: 'ALL ALONG THE WATCHTOWER',
+    },
+    {
+      author: 'AC/DC',
+      song: 'BACK IN BLACK',
+    },
+    {
+      author: 'QUEEN',
+      song: 'WE WILL ROCK YOU',
+    },
+    {
+      author: 'METALLICA',
+      song: 'ENTER SANDMAN',
+    },
+  ].forEach((item) => {
+    addItemToList(item.author + ', ' + item.song);
+  });
 }
